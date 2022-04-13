038f54f84837981f9a806e8317b616692c184352
Gaurav Pendharkar <mitugaurav15@gmail.com>
2022-04-13 05:58:05 +0530

    Flask app - Input and HTML Form

--- webapp/app.py
+++ webapp/app.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads,IMAGES
+
+import imgprocessing as i
+import pickle
 
 app = Flask(__name__)
 
-@app.route('/')
+photos =  UploadSet("photos",IMAGES)
+
+app.config["UPLOADED_PHOTOS_DEST"] = "static/images"
+configure_uploads(app,photos)
+
+@app.route('/',methods=["GET","POST"])
 def index():
     return render_template('frm.html')
 
-@app.route('/', methods=['POST'])
-def upload_file():
-    uploaded_file = request.files['file']
-    print(upload_file)
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
+@app.route("/upload",methods=["GET","POST"])
+def upload():
+    if request.method == "POST" and "photo" in request.files:
+        filename = photos.save(request.files["photo"],name="xray.jpg")
+        print(filename)
+        x = i.getPixelAsRowVector()
+        ## Load pkl file.
+        ## Predict the result.
+        ## Return the result.
+    return render_template("upload.html")
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
